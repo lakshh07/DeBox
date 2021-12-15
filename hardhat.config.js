a9e9556d238b7a5d219fc719b4f0823b48b0c0a9
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2021-12-15 10:18:40 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,16 +11,14 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   solidity: "0.8.10",
   network: {
-    // ropsten: {
-    //   url: "https://ropsten.infura.io/v3/f0ce148c25664d8c942ccc8e7063669d",
-    //   accounts: [`0x${process.env.ROPSTENKEY}`],
-    // },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/f0ce148c25664d8c942ccc8e7063669d",
+      accounts: [`0x${process.env.ROPSTENKEY}`],
+    },
     mumbai: {
-      // url: "https://ropsten.infura.io/v3/f0ce148c25664d8c942ccc8e7063669d",
+      // url: "https://mumbai.infura.io/v3/f0ce148c25664d8c942ccc8e7063669d",
       url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [
-        `0x137e99faa9dc8efdbec1516a2914e79d070f92970e7a2eae8db859d9324e7230`,
-      ],
+      accounts: [`0x${process.env.MUMBAIKEY}`]
     },
   },
 };
